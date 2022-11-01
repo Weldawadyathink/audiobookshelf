3edd4b351d091778b2447b02f8a924960f98c492
Scott Ruoti <ruoti@utk.edu>
2022-10-31 22:20:39 -0400

    Updating the devcontainer to ease initial development

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Mark the working directory as safe for use with git
+git config --global --add safe.directory $PWD
+
+# If there is no dev.js file, create it
+if [ ! -f dev.js ]; then
+  cp -a .devcontainer/dev.js .
+fi
+
+# Install packages for the server
+if [ -f package.json ]; then
+  sudo chown $(id -u):$(id -g) node_modules && npm ci
+fi
+
+# Install packages and build the client
+if [ -f client/package.json ]; then
+  (cd client && sudo chown $(id -u):$(id -g) node_modules && npm ci && npm run generate)
+fi 
